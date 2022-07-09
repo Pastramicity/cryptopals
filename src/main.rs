2cff2930c9619aea7a29ac4208eb294f49d76030
Pastramicity <chasetyates@gmail.com>
2022-07-08 18:26:25 -0700

    to s1c4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use solutions::*;
-fn main() {}
+fn main() {s1c4_single_threaded();}
 
 pub mod solutions {
     use crate::*;
+    use std::fs;
     use std::io::stdin;
     fn input() -> String {
         let mut ret = String::new();
@@ -25,7 +26,7 @@ pub mod solutions {
 
         let bytes1 = s2b::hex_2_b(&input1[..]);
         let bytes2 = s2b::hex_2_b(&input2[..]);
-        let out_bytes = manip::xor(&bytes1, &bytes2);
+        let out_bytes = manip_helper::xor_all(&bytes1, &bytes2);
         for b in &out_bytes {
             println!("{}", b);
         }
@@ -33,7 +34,27 @@ pub mod solutions {
         println!("{}", text);
     }
 
-    pub fn s1c3() {}
+    pub fn s1c4_single_threaded() {
+        let file = fs::read_to_string("s1c4.txt").expect("Couldn't read s1c4.txt");
+        let mut strings = Vec::new();
+        let mut scores = Vec::new();
+        let mut max_score = 0;
+        let mut max_score_index = 0;
+        for line in file.split("\n") {
+            let line_decoded = s2b::hex_2_b(line);
+            for i in 0..=255u8 {
+                let line_xored = manip_helper::xor_one_2_string(&line_decoded, &i);
+                let score = manip_helper::english_checker(&line_xored);
+                if score > max_score{
+                    max_score = score;
+                    max_score_index = scores.len();
+                }
+                scores.push(score);
+                strings.push(line_xored);
+            }
+        }
+        println!("{}", strings[max_score_index]);
+    }
 }
 
 // string to bytes
@@ -52,13 +73,13 @@ pub mod s2b {
 }
 
 pub mod b2s {
-    use crate::manip;
+    use crate::manip_helper;
     // 6 bits per hex digit
     // no overflow
     pub fn b_2_b64(bytes: &Vec<u8>) -> String {
         let mut ret = String::new();
         //take bytes in chunks of 3 to turn into 4 digits of Base64
-        let b64_bytes = manip::rechunk(64, bytes);
+        let b64_bytes = manip_helper::rechunk(64, bytes);
         for byte in b64_bytes {
             ret.push(match byte {
                 0..=25 => byte + b'A',
@@ -85,7 +106,7 @@ pub mod b2s {
         ret
     }
 }
-pub mod manip {
+pub mod manip_helper {
     pub fn rechunk(base: u32, bytes: &Vec<u8>) -> Vec<u8> {
         let mut ret: Vec<u8> = Vec::new();
         let bpd = f32::log2(base as f32) as u8; //bits per digit
@@ -116,7 +137,7 @@ pub mod manip {
         ret
     }
 
-    pub fn xor(bytes1: &Vec<u8>, bytes2: &Vec<u8>) -> Vec<u8> {
+    pub fn xor_all(bytes1: &Vec<u8>, bytes2: &Vec<u8>) -> Vec<u8> {
         let mut ret = Vec::new();
         assert_eq!(bytes1.len(), bytes2.len());
         let len = bytes1.len();
@@ -126,10 +147,25 @@ pub mod manip {
         ret
     }
 
-    fn lcm(first: u32, second: u32) -> u32 {
+    pub fn xor_one(bytes: &Vec<u8>, byte: &u8) -> Vec<u8> {
+        let mut ret = Vec::new();
+        for b in bytes {
+            ret.push(b ^ byte);
+        }
+        ret
+    }
+    pub fn xor_one_2_string(bytes: &Vec<u8>, byte: &u8) -> String {
+        let mut ret = String::new();
+        for b in bytes {
+            ret.push((b ^ byte) as char);
+        }
+        ret
+    }
+
+    pub fn lcm(first: u32, second: u32) -> u32 {
         first * second / gcd(first, second)
     }
-    fn gcd(first: u32, second: u32) -> u32 {
+    pub fn gcd(first: u32, second: u32) -> u32 {
         let mut max = first;
         let mut min = second;
         if min > max {
@@ -147,6 +183,25 @@ pub mod manip {
             max = min;
             min = res;
         }
+    }
+    // scores a string based on how likely it is to be language, may be changed later
+    pub fn english_checker(string: &String) -> i64 {
+        let mut ret = 0;
+        for char in string.chars(){
+            ret += match char{
+                ' ' => 10,
+                'e' => 8,
+                't' => 7,
+                'a' => 7,
+                'i' => 6,
+                'o' => 6,
+                'n' | 's' | 'h' | 'r' => 5,
+                'a'..='z' => 4,
+                '0'..='9' | ',' | '.' | '!' | '?' => 3,
+                _ => 0
+            }
+        }
+        ret
     }
 }
 
